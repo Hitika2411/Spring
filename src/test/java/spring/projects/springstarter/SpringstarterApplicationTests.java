141e3eb09786c4b9eb163ccb638dbd4dacf4f162
Hitika <hitika.asr@gmail.com>
2025-01-21 23:47:06 +0530

    Spring Initialised

---
+++ src/test/java/spring/projects/springstarter/SpringstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.projects.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
