a50b7752c8b55493849121bd6cc77f1e8ba5553a
Hitika <hitika.asr@gmail.com>
2025-01-27 22:21:58 +0530

    adding routes

--- src/main/java/spring/projects/springstarter/Controller/HomeController.java
+++ src/main/java/spring/projects/springstarter/Controller/HomeController.java
@@ -13,4 +13,14 @@ public class HomeController {
     public String home(Model model){
         return "home";
     }
+    @GetMapping("/about")
+    
+    public String about(Model model){
+        return "about";
+    }
+    @GetMapping("/book")
+    
+    public String book(Model model){
+        return "book";
+    }
 }
