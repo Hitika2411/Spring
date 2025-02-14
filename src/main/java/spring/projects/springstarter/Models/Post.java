4d7c74366f3b73566d595468a428352c67be2a4e
Hitika <hitika.asr@gmail.com>
2025-02-14 23:34:46 +0530

    mapping account model to post model using annotations

--- src/main/java/spring/projects/springstarter/Models/Post.java
+++ src/main/java/spring/projects/springstarter/Models/Post.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,4 +22,7 @@ public class Post {
     @Column (columnDefinition = "TEXT")
     private String Text;
     private String Title;
+    @ManyToOne()
+    @JoinColumn(nullable = true,columnDefinition = "accountId",referencedColumnName = "id")
+    private Account account;
 }
