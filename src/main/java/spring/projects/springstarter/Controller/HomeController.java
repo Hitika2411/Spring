399983de0597e03eefe447bf063ece54c98a447d
Hitika <hitika.asr@gmail.com>
2025-02-14 22:47:30 +0530

    displaying seed data on home screen

--- src/main/java/spring/projects/springstarter/Controller/HomeController.java
+++ src/main/java/spring/projects/springstarter/Controller/HomeController.java
@@ -1,16 +1,23 @@
 package spring.projects.springstarter.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import ch.qos.logback.core.model.Model;
+//import ch.qos.logback.core.model.Model;
+import spring.projects.springstarter.Models.Post;
+import spring.projects.springstarter.Service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private PostService ps;
     @GetMapping("/home")
-    
     public String home(Model model){
+        List<Post> posts=ps.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 
